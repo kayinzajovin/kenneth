ee4217e2e93d5d0a24c964eedd261be00987e256
dorcustumulamye <dorcustumulamye@gmail.com>
2026-05-21 18:58:07 +0300

    login authentication

--- parkease/park/urls.py
+++ parkease/park/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('booking/<int:booking_id>/', views.booking_details, name='booking_details'),
     path('booking/<int:booking_id>/pay/', views.make_payment, name='make_payment'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/admin/', views.admin_dashboard, name='admin_dashboard'),
+    path('dashboard/driver/', views.driver_dashboard, name='driver_dashboard'),
+    path('dashboard/attendant/', views.attendant_dashboard, name='attendant_dashboard'),
 ]
